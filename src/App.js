b9ae5fc636ffb2273525d9c5ceb20ac6f5e0024f
Kellie Matteson <kelliematteson@yahoo.com>
2021-03-23 21:08:53 -0700

    front end created and data displays

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [gives, setGives] = useState([]);
+
+  useEffect(() => {
+    const makeAPICall = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/gives')
+        const data = await res.json();
+        setGives(data.gives);
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    makeAPICall()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Let's see some data!</h3>
       </header>
+      <div className="gives">
+        <ul>
+          {gives.map((give, id) => {
+            return (
+              <div className="give-card-image">
+
+                <div className="give-name">{give.give_name}</div>
+
+              </div>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
